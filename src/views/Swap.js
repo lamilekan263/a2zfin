90af056dd687a374f2adc7cc05f8987201115ca0
lamilekan263 <lamilekan263@gmail.com>
2021-09-22 08:42:37 +0100

    on it

--- src/views/Swap.js
+++ src/views/Swap.js
@@ -1,15 +1,6 @@
 import React from "react";
 // reactstrap components
-import {
-
-  Card,
-  CardHeader,
-  CardBody,
-  
-  Badge,
-  Row,
-  Col,
-} from "reactstrap";
+import { Card, CardHeader, CardBody, Badge, Row, Col } from "reactstrap";
 
 function Swap() {
   return (
@@ -22,17 +13,29 @@ function Swap() {
               <CardHeader>
                 <div className="d-flex justify-content-between">
                   <div>
-                    <p>HyperDEX</p>
-                    <span>best_rates_on_dex_market</span>
+                    <p className="font-weight-bold">A2ZFIN</p>
+                    <span className="mt-n5 text-muted">
+                      best_rates_on_dex_market
+                    </span>
                   </div>
                   <div>
-                    <Badge>arrow</Badge>
+                    <Badge disabled color="light" className="ml-2 ">
+                      <ion-icon
+                        name="reload-outline"
+                        style={{ height: "25px", width: "30px" }}
+                      ></ion-icon>
+                    </Badge>
 
-                    <Badge className="ml-2">switch</Badge>
+                    <Badge color="light" className="ml-2 ">
+                      <ion-icon
+                        name="options-outline"
+                        style={{ height: "25px", width: "30px" }}
+                      ></ion-icon>
+                    </Badge>
                   </div>
                 </div>
               </CardHeader>
-              <CardBody className="my-3">
+              <CardBody className="my-1">
                 <form>
                   <div className="mb-3 form-field">
                     <div className="field-label">
@@ -61,10 +64,13 @@ function Swap() {
                         />
 
                         <div className="border">
-                          <div className="d-flex justify-content-center align-items-center px-3">
+                           <Badge
+                            color="light"
+                            className="d-flex justify-content-center align-items-center  px-3"
+                          >
                             <div
-                              className="token-logo "
-                              style={{ height: "30px" }}
+                              className="token-logo"
+                              style={{ height: "25px", width: "30px" }}
                             >
                               <img
                                 src={
@@ -75,7 +81,7 @@ function Swap() {
                               />
                             </div>
                             <span>ETH</span>
-                          </div>
+                          </Badge>
                         </div>
                       </div>
                     </div>
@@ -108,21 +114,25 @@ function Swap() {
                         />
 
                         <div className="border">
-                          <div className="d-flex justify-content-center align-items-center px-3">
+                          <Badge
+                            color="light"
+                            className="d-flex justify-content-center align-items-center  px-3"
+                          >
                             <div
-                              className="token-logo "
-                              style={{ height: "30px" }}
+                              className="token-logo"
+                              style={{ height: "25px", width: "30px" }}
                             >
                               <img
                                 src={
-                                  require("assets/currencies/plasma.svg").default
+                                  require("assets/currencies/plasma.svg")
+                                    .default
                                 }
                                 alt="eth"
                                 className="h-100 w-100"
                               />
                             </div>
                             <span>PPAY</span>
-                          </div>
+                          </Badge>
                         </div>
                       </div>
                     </div>
